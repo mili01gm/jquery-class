bd963c951cb47a0ab993b31c44ebcb4bca3a43f8
Luz Gutierrez <mili01gm@gmail.com>
2017-06-01 17:30:01 -0500

    Ejemplo jQuery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(document).ready();
+// $(document).ready(function(){
+//    alert("cargó jQuery");
+// });
+
+$(() => {
+    $("a").on('click', (e) => {
+        e.preventDefault();
+        $('.brand-popup').toggle();
+    });
+    $(".brand-popup .popup-close").on('click', (e) => {
+        e.preventDefault();
+        $('.brand-popup').hide();
+    })
+});
